c9ff32a8a2e3a996860f7c1f7a18c215d89cd4d2
Sergio Tabarini <tabarinisergio@gmail.com>
2015-01-20 12:26:29 -0600

    First commit

--- spec/spec.js
+++ spec/spec.js
@@ -3,5 +3,25 @@ describe("First jasmine project", function() {
 
     expect(helloWorld()).toBe("hello world");
   });
-
+  it("It should write Jasmine", function() {
+	
+	expect(titles).toBe("Jasmine");
+	expect(titles).not.toBe(" ");
+  });
+  it("It should include in the title Jasmine", function() {
+	
+	expect(titles).toMatch(/jasmine/i);
+  });
+  it("It should verify the sum of two numbers", function() {
+	
+	expect(sum(2 , 2)).toBe(4);
+	expect(sum(3 , 6)).toBe(9);
+	expect(sum(-50 , 1000)).toBe(950);
+	expect(sum("50" , "400")).toBe(450);
+	expect(sum("This is a string" , 50)).toBe(null);
+	expect(sum(50 , 80)).toBeGreaterThan(100);
+	expect(sum(50 , 100)).toBeLessThan(160);
+  });
 });
+
+
--- src/script.js
+++ src/script.js
@@ -1,3 +1,20 @@
+var titles = "Jasmine";
+var num1 = "";
+var num2 = "";
+
 function helloWorld() {
   return "hello world";
 }
+
+function title() {
+	document.write("<h2>" + titles + "</h2>" )
+};
+title();
+
+function sum(num1, num2) {
+	if(isNaN(num1)||isNaN(num2)) {
+		return null;
+	}else{
+		return parseInt(num1) + parseInt(num2);
+	};
+};
